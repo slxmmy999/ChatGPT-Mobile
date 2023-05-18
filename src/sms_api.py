cb2934cf256e98eb55f8c8f988e2b9798681b7a3
Sam Coan <62761327+slxmmy999@users.noreply.github.com>
2023-05-17 19:23:24 -0600

    keep smtp server alive

--- src/sms_api.py
+++ src/sms_api.py
@@ -2,19 +2,22 @@ import smtplib
 from setup import gmail_password, email, carrier
 from log import AppLogger
 
-logger = AppLogger()
+class MessageService:
+    def __init__(self):
+        self.logger = AppLogger()
 
-EMAIL = email
-PASSWORD = gmail_password
- 
-def send_message(phone_number, message):
-    logger.SMS_LOG_EVENT("Preparing to send SMS", "info")
-    recipient = phone_number + carrier
-    auth = (EMAIL, PASSWORD)
- 
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(auth[0], auth[1])
- 
-    server.sendmail(auth[0], recipient, message)
-    logger.SMS_LOG_EVENT("SMS sent", "info")
+        self.EMAIL = email
+        self.PASSWORD = gmail_password
+
+        auth = (self.EMAIL, self.PASSWORD)
+    
+        self.server = smtplib.SMTP("smtp.gmail.com", 587)
+        self.server.starttls()
+        self.server.login(auth[0], auth[1])
+    
+    def send_message(self, phone_number, message):
+        self.logger.SMS_LOG_EVENT("Preparing to send SMS", "info")
+        recipient = phone_number + carrier
+    
+        self.server.sendmail(self.EMAIL, recipient, message)
+        self.logger.SMS_LOG_EVENT("SMS sent", "info")
